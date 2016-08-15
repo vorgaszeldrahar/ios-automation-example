96a24882cae4fe5016dd780e2f916902496882cd
Brandon Tweed <citrixbtweed@gmail.com>
2016-08-15 14:11:32 -0400

    Use python2.7 instead of python 3.4

--- runtests.sh
+++ runtests.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+# Kill Appium if it's already running
+APPIUM=$(ps -ef | grep "node appium" | head -1 | cut -d " " -f 4)
+if [ ${APPIUM} -ne '' ]; then
+    kill $(APPIUM)
+fi
+
 # Launch Appium using the same command as the App
-'/Applications/Appium.app/Contents/Resources/node/bin/node' /Applications/Appium.app/Contents/Resources/node_modules/appium/build/lib/main.js --address "127.0.0.1" --session-override --debug-log-spacing --platform-version "8.1" --platform-name "iOS" --show-ios-log --default-device &
+'/Applications/Appium.app/Contents/Resources/node/bin/node' /Applications/Appium.app/Contents/Resources/node_modules/appium/build/lib/main.js --address "127.0.0.1" --session-override --debug-log-spacing --platform-version "9.3" --platform-name "iOS" --show-ios-log --default-device &
 
 # Remove the old virtualenv
 rm -rf ./env/
 
 # Create a fresh one
-virtualenv -p python3.4 env
+virtualenv -p python2.7 env
 
 # activate the virtualenv
 source env/bin/activate
